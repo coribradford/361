aed7b441c65aeb00a21b827c2a777e77205893a5
cori <bradfcor@oregonstate.edu>
2021-07-23 13:53:31 -0700

    cleaning repeat code

--- image_scraper.py
+++ image_scraper.py
@@ -4,6 +4,8 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpim
 
 page = requests.get('https://store.steampowered.com/app/238960/Path_of_Exile/?snr=1_4_4__118')
 # test page
@@ -11,7 +13,10 @@ soup = BeautifulSoup(page.content, 'html.parser')
 images = soup.findAll('img')
 
 
-for item in soup.findAll('img'):
+for item in images:
     print(item['src'])
 
+# img = mpim.imread(images[0])
+# imgplot = plt.imshow(img)
+# plt.show()
 # currently grabs and prints all image urls - make this useful somehow
