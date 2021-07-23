7d84886b71da753bfbc53126374ddc4afa772dbb
cori <bradfcor@oregonstate.edu>
2021-07-23 13:49:01 -0700

    grabbing image urls successfully

--- image_scraper.py
+++ image_scraper.py
@@ -4,3 +4,14 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+
+page = requests.get('https://store.steampowered.com/app/238960/Path_of_Exile/?snr=1_4_4__118')
+# test page
+soup = BeautifulSoup(page.content, 'html.parser')
+images = soup.findAll('img')
+
+
+for item in soup.findAll('img'):
+    print(item['src'])
+
+# currently grabs and prints all image urls - make this useful somehow
