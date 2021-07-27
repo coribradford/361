68d0c0d4dd71a6418f803045c1be75088749bce3
cori <bradfcor@oregonstate.edu>
2021-07-27 13:41:54 -0700

    built hello route

--- image_scraper.py
+++ image_scraper.py
@@ -2,9 +2,12 @@
 # CS361 Summer 2021
 
 from bs4 import BeautifulSoup
+from flask import Flask
+from flask import jsonify
 import requests
 import re
 
+app = Flask(__name__)
 
 def img_scraper(keyword):
     keyword = keyword.replace(" ", "_")
@@ -16,3 +19,8 @@ def img_scraper(keyword):
         if re.search('wikipedia/.*/thumb/', image.get('src')) and not re.search('.svg', image.get('src')):
             return image.get('src')
     return "could not find image"
+
+@app.route('/')
+def hello():
+    """Return http greeting"""
+    return "Welcome to Cori's Image scraper service!"
