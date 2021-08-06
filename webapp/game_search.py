e2cd6389f74acfdc3935d001845c7121e4986afa
cori <bradfcor@oregonstate.edu>
2021-08-06 14:27:40 -0700

    changed favicon, include source info

--- webapp/game_search.py
+++ webapp/game_search.py
@@ -7,19 +7,19 @@ root = Tk()
 
 def click_search():
     game_name_label_widget = Label(root, text=entry_widget.get())
-    game_name_label_widget.pack()
+    entry_widget.delete(0, END)
+    game_name_label_widget.grid(row=2, column=0)
 
-
-title_label_widget = Label(root, text="Game Search Engine")
+root.title("Game Search Engine")
+root.iconbitmap('games.ico')
 instructions_label_widget = Label(root, text="Enter a game to search")
-title_label_widget.pack()
-instructions_label_widget.pack()
+instructions_label_widget.grid(row=1, column=0)
 
 entry_widget = Entry(root, width=50)
-entry_widget.pack()
+entry_widget.grid(row=0, column=0)
 
 
 button_widget = Button(root, text="Search", command=click_search)
-button_widget.pack()
+button_widget.grid(row=0, column=1)
 
 root.mainloop()
