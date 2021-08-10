f23bd721d91dfc70285c9e51658914d7dcb9d0d7
cori <bradfcor@oregonstate.edu>
2021-08-10 13:43:05 -0700

    changing to flask/html over tkinter

--- webapp/game_search.py
+++ webapp/game_search.py
@@ -1,25 +1,36 @@
 # Corinne Bradford
 # CS361 Summer 2021
 
-from tkinter import *
+# from tkinter import *
 
-root = Tk()
+# root = Tk()
 
-def click_search():
-    game_name_label_widget = Label(root, text=entry_widget.get())
-    entry_widget.delete(0, END)
-    game_name_label_widget.grid(row=2, column=0)
+# def click_search():
+#     game_name_label_widget = Label(root, text=entry_widget.get())
+#     entry_widget.delete(0, END)
+#     game_name_label_widget.grid(row=2, column=0)
 
-root.title("Game Search Engine")
-root.iconbitmap('games.ico')
-instructions_label_widget = Label(root, text="Enter a game to search")
-instructions_label_widget.grid(row=1, column=0)
+# root.title("Game Search Engine")
+# root.iconbitmap('games.ico')
+# instructions_label_widget = Label(root, text="Enter a game to search")
+# instructions_label_widget.grid(row=1, column=0)
 
-entry_widget = Entry(root, width=50)
-entry_widget.grid(row=0, column=0)
+# entry_widget = Entry(root, width=50)
+# entry_widget.grid(row=0, column=0)
 
 
-button_widget = Button(root, text="Search", command=click_search)
-button_widget.grid(row=0, column=1)
+# button_widget = Button(root, text="Search", command=click_search)
+# button_widget.grid(row=0, column=1)
 
-root.mainloop()
+# root.mainloop()
+
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
