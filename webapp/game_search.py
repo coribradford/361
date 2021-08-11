43d3ef7d9bd86378be507220f476065f5626cbbf
cori <bradfcor@oregonstate.edu>
2021-08-10 20:10:16 -0700

    better error handling

--- webapp/game_search.py
+++ webapp/game_search.py
@@ -14,13 +14,13 @@ def wiki_scraper(keyword):
     try:
         length = len(keyword)
         summary = wikipedia.summary(keyword, auto_suggest=False, sentences=6)
-        title = summary[:length]
+        title = summary[:(length+1)]
         returns = [title, summary]
         return returns
     except wikipedia.exceptions.PageError:
-        return
+        return [-1, -1]
     except wikipedia.exceptions.DisambiguationError as e:
-        return
+        return [-1, -1]
 
 def img_scraper(keyword):
     keyword = keyword.replace(" ", "_")
@@ -31,13 +31,11 @@ def img_scraper(keyword):
         src = image.get('src')
         if re.search('wikipedia/.*/thumb/', src) and not re.search('.svg', src):
             return src
-    return -1
+    return
 
 def video_id_lookup(keyword):
     try:
-        keyword = keyword.replace(" ", "+")
-        print(keyword)
-        keyword = keyword + "+trailer"
+        keyword = keyword.replace(" ", "+") + "+game+trailer"
         url = "https://www.youtube.com/results?search_query=" + keyword
         html = urllib.request.urlopen(url)
         video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
@@ -55,12 +53,18 @@ def search():
     if request.method == "POST":
         search_info = request.form["search_input"]
         output = wiki_scraper(search_info)
+        if output is not None:
+            title = output[0]
+            summary = output[1]
+        else:
+            title = "hold"
+            summary = "hold"
         image = img_scraper(search_info)
         id = video_id_lookup(search_info)
         payload = {"videoid": id}
         response = requests.get("http://flip1.engr.oregonstate.edu:65334/embedlink", params=payload)
         link = response.text
-        return render_template("search.html", game_title=output[0], content=output[1], picture=image, embed=link)
+        return render_template("search.html", game_title=title, content=summary, picture=image, embed=link)
     else:
         return render_template("search.html")
 
