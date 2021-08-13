2b8aa61149bd4e3091d32b12b53209404dd8a4e2
cori <bradfcor@oregonstate.edu>
2021-08-13 14:47:58 -0700

    featured article working

--- webapp/game_search.py
+++ webapp/game_search.py
@@ -1,3 +1,38 @@
+
+def steam_id_lookup(keyword):
+    try:
+        steam_id = 0
+        url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+        page = urllib.request.urlopen(url)
+        data = json.loads(page.read())
+        apps = data["applist"]["apps"]
+        for app in apps:
+            if app["name"] == keyword:
+                steam_id = app["appid"]
+        return steam_id
+    except urllib.error.HTTPError:
+        return steam_id
+
+def steam_price_lookup(app_id):
+    try:
+        steam_price = "Free to play"
+        if app_id == 0:
+            return steam_price
+        url = "https://store.steampowered.com/api/appdetails?appids=" + str(app_id)
+        page = urllib.request.urlopen(url)
+        data = json.loads(page.read())
+        app_id = str(app_id)
+        info = data[app_id]["data"]["price_overview"]
+        for stuff in info:
+            steam_price = stuff["final_formatted"]
+        return steam_price
+    except KeyError:
+        return steam_price
+    except urllib.error.HTTPError:
+        return steam_price
+
+
+
 # Corinne Bradford
 # CS361 Summer 2021
 
@@ -7,6 +42,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import urllib
+import datetime
 import json
 
 app = Flask(__name__)
@@ -24,8 +60,17 @@ def wiki_scraper(keyword):
         return returns
     except wikipedia.exceptions.PageError:
         return [-1, -1, -1]
-    except wikipedia.exceptions.DisambiguationError as e:
+    except wikipedia.exceptions.DisambiguationError:
         return [-1, -1, -1]
+
+def featured_wiki_title():
+    today = datetime.datetime.now()
+    date = today.strftime('%Y/%m/%d')
+    url = 'https://api.wikimedia.org/feed/v1/wikipedia/en/featured/' + date
+    page = urllib.request.urlopen(url)
+    data = json.loads(page.read())
+    return data['tfa']['title']
+
 
 def img_scraper(keyword):
     keyword = keyword.replace(" ", "_")
@@ -48,7 +93,6 @@ def video_id_lookup(keyword):
     except urllib.error.HTTPError:
         return
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -57,9 +101,7 @@ def home():
 def search():
     if request.method == "POST":
         if request.form.get("featured") == "Featured Article":
-            return render_template("check.html")
-        elif request.values == "Random Article":
-            return render_template("check.html")
+            return redirect(url_for(featured))
         else:
             search_info = request.form["search_input"]
             output = wiki_scraper(search_info)
@@ -73,13 +115,29 @@ def search():
             link = response.text
             google_keyword = title.replace(" ", "+")
             google_url = "https://www.google.com/search?q=" + google_keyword
-            return render_template("search.html", game_title=title, content=summary, wiki=wiki_url, picture=image, embed=link, google=google_url)
+            return render_template("search.html", title=title, content=summary, wiki=wiki_url, picture=image, embed=link, google=google_url)
     else:
         return render_template("index.html")
 
 @app.route("/instructions")
 def instructions():
     return render_template("instructions.html")
+
+@app.route("/featured", methods=["GET", "POST"]) 
+def featured():
+    search_info = featured_wiki_title()
+    output = wiki_scraper(search_info)
+    title = output[0]
+    summary = output[1]
+    wiki_url = output[2]
+    image = img_scraper(title)
+    video_id = video_id_lookup(title)
+    payload = {"videoid": video_id}
+    response = requests.get("http://flip1.engr.oregonstate.edu:65334/embedlink", params=payload)
+    link = response.text
+    google_keyword = title.replace(" ", "+")
+    google_url = "https://www.google.com/search?q=" + google_keyword
+    return render_template("featured.html", title=title, content=summary, wiki=wiki_url, picture=image, embed=link, google=google_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
